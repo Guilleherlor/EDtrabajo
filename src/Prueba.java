8b9c4971eb08b97a33c986ed5150ab6f34e2d46b
Guillermo Hernandez <guilleherlor@outlook.com>
2021-03-28 15:03:34 +0200

    Introducida la comprobacion de si el numero es primo

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class Prueba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Introduza un numero");
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
 	}
 
 }
